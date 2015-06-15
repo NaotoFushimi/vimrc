919ff59538b071e33743f9eaf1433da65501276c
fushiminaoto <wano_fushimi@icloud.com>
2015-06-16 02:33:58 +0900

    setup.pl整備

--- setup.pl
+++ setup.pl
@@ -1,4 +1,7 @@
-` mkdir -p ~/.vim/colors`;
+`cp .vimrc ~/`;
+`cp .zshrc ~/`;
+`source .zshrc`;
+`mkdir -p ~/.vim/colors`;
 `git clone https://github.com/tomasr/molokai`;
 `mv molokai/colors/molokai.vim ~/.vim/colors/`;
 
